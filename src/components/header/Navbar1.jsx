1f46acef5692758f7f91cab5ebe5a552f1df1ae3
budi <gilangbb1010@gmail.com>
2023-06-07 22:09:28 +0700

    navbar

---
+++ src/components/header/Navbar1.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Navbar, Container, Form, Nav, Button } from "react-bootstrap";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+const Navbar1 = () => {
+  return (
+    <Navbar className="navbar">
+      <Container>
+        <Navbar.Brand as={Link} to={"/"}>
+          <img
+            src="../../../img/logo.svg"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+        </Navbar.Brand>
+        <Nav className="">
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Cari disini .."
+              className="search"
+              aria-label="Search"
+            />
+            <Button variant="transparent" className="icon-search">
+              <img src="../../../img/search-ikon.svg" alt="search" />
+            </Button>
+          </Form>
+        </Nav>
+        <Button variant="transparent" as={Link} to={"/login"}>
+          <img
+            src="../../../img/masuk.svg"
+            alt="Logo"
+            className="icon-search"
+          />
+        </Button>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navbar1;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar1 from "../components/header/Navbar1";
+
+const Dashboard = () => {
+  return <Navbar1 />;
+};
+
+export default Dashboard;
