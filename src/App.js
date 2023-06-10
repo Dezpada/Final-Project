4965503e0e2d3c05d7a7b94be18edaeb132c70c1
Daniel Nugroho Simanjuntak <107357522+Dezpada@users.noreply.github.com>
2023-06-10 21:00:42 +0700

    F add login and register (#5)
    * f:add Login.jsx
    
    * f:add Login and Register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
