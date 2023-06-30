b2cc045c4fceb2cdb864539319bc1db989585673
Daniel <devilwhitedemon@gmail.com>
2023-06-30 17:44:11 +0700

    update checkout

--- src/components/checkout/CheckoutCol2.jsx
+++ src/components/checkout/CheckoutCol2.jsx
@@ -1,19 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import moment from "moment";
+import "moment/locale/id";
+moment.locale("id");
 
 function CheckoutCol2() {
+  const [flight, setFlight] = useState("");
+  const [departureAirport, setDepartureAirport] = useState("");
+  const [arrivalAirport, setArrivalAirport] = useState("");
+  const [airplane, setAirplane] = useState("");
+  const [airline, setAirline] = useState("");
+  const [adultPassenger, setAdultPassenger] = useState("");
+  const [kidPassenger, setKidPassenger] = useState("");
+  const [babyPassenger, setBabyPassenger] = useState("");
+  const [priceAdults, setPriceAdults] = useState();
+  const [priceKids, setPriceKids] = useState();
+  const [priceBaby, setPriceBaby] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const response = await axios.get(
+          `https://final-project-production-b6fe.up.railway.app/flight/${params.id}`
+        );
+        setFlight(response.data.data);
+        setDepartureAirport(response.data.data.departureAirport);
+        setArrivalAirport(response.data.data.arrivalAirport);
+        setAirplane(response.data.data.airplane);
+        setAirline(response.data.data.airplane.airline);
+      } catch (error) {
+        alert(error);
+      }
+    }
+
+    if (params?.id) {
+      fetchPost().then(calculatePricePassengers()).then(calculatePriceTotal());
+    }
+
+    async function calculatePricePassengers() {
+      await fetchPost();
+      const adult = window.localStorage.getItem("adult_passengers");
+      setAdultPassenger(adult);
+      const kid = window.localStorage.getItem("kid_passengers");
+      setKidPassenger(kid);
+      const baby = window.localStorage.getItem("baby_passengers");
+      setBabyPassenger(baby);
+
+      setPriceAdults(adultPassenger * flight.price);
+      setPriceKids(kidPassenger * flight.price);
+      setPriceBaby(babyPassenger * 0);
+    }
+    async function calculatePriceTotal() {
+      await fetchPost();
+      await calculatePricePassengers();
+      setTotalPrice(priceAdults + priceKids + priceBaby + 300000);
+    }
+  }, [params]);
+
   return (
     <Container className="my-3">
       <div className="d-flex">
         <div className="d-flex flex-column">
           <h6 className="text-purple fw-semibold fs-14">Detail Penerbangan</h6>
-          <h6 className="fw-bold fs-16">07:00</h6>
-          <h6 className="fw-normal fs-14">3 Maret 2023</h6>
+          <h6 className="fw-bold fs-16">{flight.departure_time}</h6>
+          <h6 className="fw-normal fs-14">
+            {moment(flight.flight_date).format("LL")}
+            {/* 3 Maret 2023 */}
+          </h6>
           <div className="d-flex">
-            <h6 className="fw-semibold fs-14">Soekarno Hatta</h6>
+            <h6 className="fw-semibold fs-14">
+              {departureAirport.name}
+              {/* Soekarno Hatta */}
+            </h6>
             <h6 className="fw-semibold fs-14 mx-2">-</h6>
-            <h6 className="fw-semibold fs-14">Terminal 1A Domestik</h6>
+            <h6 className="fw-semibold fs-14">
+              {flight.departure_terminal_name}
+              {/* Terminal 1A Domestik */}
+            </h6>
           </div>
         </div>
         <h6 className="ms-auto my-auto fw-bold fs-12 text-purple">
@@ -23,33 +91,54 @@ function CheckoutCol2() {
       <div className="border-bottom w-50 mx-auto my-2"></div>
       <div className="d-flex">
         <div className="me-2 my-auto">
-          <img src="/img/logo_leaf.svg" alt="" />
+          <img src={airline.icon_url} alt="" width={50} />
+          {/* "/img/logo_leaf.svg" */}
         </div>
         <div className="d-flex flex-column">
           <div>
             <div className="d-flex">
-              <h6 className="fw-bold fs-14">Jet Air</h6>
+              <h6 className="fw-bold fs-14">
+                {airline.name}
+                {/* Jet Air */}
+              </h6>
               <h6 className="fw-bold fs-14 mx-2">-</h6>
-              <h6 className="fw-bold fs-14">Economy</h6>
+              <h6 className="fw-bold fs-14">
+                {flight.class}
+                {/* Economy */}
+              </h6>
             </div>
             <div className="d-flex">
-              <h6 className="fw-bold fs-14">JT</h6>
-              <h6 className="fw-bold fs-14 mx-2">-</h6>
-              <h6 className="fw-bold fs-14">203</h6>
+              <h6 className="fw-bold fs-14">
+                {flight.flight_number}
+                {/* JT */}
+              </h6>
+              {/* <h6 className="fw-bold fs-14 mx-2">-</h6>
+              <h6 className="fw-bold fs-14">203</h6> */}
             </div>
           </div>
           <h6 className="fw-bold fs-14 mt-2">Informasi:</h6>
-          <h6 className="fw-normal fs-14">Baggage 20 Kg</h6>
-          <h6 className="fw-normal fs-14">Cabin Baggage 7 Kg</h6>
+          <h6 className="fw-normal fs-14">Baggage {flight.free_baggage} Kg</h6>
+          <h6 className="fw-normal fs-14">
+            Cabin Baggage {flight.cabin_baggage} Kg
+          </h6>
           <h6 className="fw-normal fs-14">in Flight Entertainment</h6>
         </div>
       </div>
       <div className="border-bottom w-50 mx-auto my-2"></div>
       <div className="d-flex">
         <div className="d-flex flex-column">
-          <h6 className="fw-bold fs-14">07:00</h6>
-          <h6 className="fw-normal fs-14">3 Maret 2023</h6>
-          <h6 className="fw-semibold fs-14">Melbourne International Airport</h6>
+          <h6 className="fw-bold fs-14">
+            {flight.arrival_time}
+            {/* 07:00 */}
+          </h6>
+          <h6 className="fw-normal fs-14">
+            {moment(flight.flight_date).format("LL")}
+            {/* 3 Maret 2023 */}
+          </h6>
+          <h6 className="fw-semibold fs-14">
+            {arrivalAirport.name}
+            {/* Melbourne International Airport */}
+          </h6>
         </div>
         <h6 className="ms-auto my-auto fw-bold fs-12 text-purple">
           Kedatangan
@@ -59,15 +148,17 @@ function CheckoutCol2() {
       <h6 className="fw-bold fs-14">Rincian Harga</h6>
       <div className="d-flex flex-row justify-content-between">
         <div className="d-flex flex-column">
-          <h6 className="fw-normal fs-14">2 Adults</h6>
-          <h6 className="fw-normal fs-14">1 Baby</h6>
+          <h6 className="fw-normal fs-14">{adultPassenger} Adults</h6>
+          <h6 className="fw-normal fs-14">{kidPassenger} Kids</h6>
+          <h6 className="fw-normal fs-14">{babyPassenger} Baby</h6>
           <h6 className="fw-normal fs-14">Tax</h6>
         </div>
         <div className="d-flex flex-column">
           <h6 className="fw-bold fs-14 text-end"> </h6>
-          <h6 className="fw-normal fs-14 text-end">IDR 9.550.000</h6>
-          <h6 className="fw-normal fs-14 text-end">IDR 0</h6>
-          <h6 className="fw-normal fs-14 text-end">IDR 300.000</h6>
+          <h6 className="fw-normal fs-14 text-end">IDR {priceAdults}</h6>
+          <h6 className="fw-normal fs-14 text-end">IDR {priceKids}</h6>
+          <h6 className="fw-normal fs-14 text-end">IDR {priceBaby}</h6>
+          <h6 className="fw-normal fs-14 text-end">IDR 300000</h6>
         </div>
       </div>
       <div className="border-bottom w-50 mx-auto my-2"></div>
@@ -76,7 +167,7 @@ function CheckoutCol2() {
           <h6 className="fw-bold fs-20">Total</h6>
         </div>
         <h6 className="ms-auto my-auto fw-bold fs-20 text-purple">
-          IDR 9.850.000
+          IDR {totalPrice}
         </h6>
       </div>
       <div className=" mx-auto ">
